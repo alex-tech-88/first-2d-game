da99ed20ea714d64fa7b0b264bc977b8b8a31a27
Alex <alexlegosergeev@yandex.ru>
2026-02-19 20:29:50 +0300

    Added sound and hit animation

--- first-game/scripts/player.gd
+++ first-game/scripts/player.gd
@@ -1,33 +1,42 @@
 extends CharacterBody2D
 
-
 const SPEED = 130.0
 const JUMP_VELOCITY = -300.0
 
-# Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+var is_dead := false #death state
 
 @onready var animated_sprite = $AnimatedSprite2D
+@onready var hit_sound = $hit_sound
+
 
 func _physics_process(delta):
-	# Add the gravity.
+	if is_dead:
+		# Sliding after hit
+		velocity.x = move_toward(velocity.x, 0, 600 * delta) # slowed down sliding after death a little
+		if not is_on_floor():
+			velocity.y += gravity * delta
+		move_and_slide()
+		return
+
+	# Gravity
 	if not is_on_floor():
 		velocity.y += gravity * delta
 
-	# Handle jump.
+	# Jump
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
-	# Get the input direction: -1, 0, 1
+	# Move
 	var direction = Input.get_axis("move_left", "move_right")
-	
-	# Flip the Sprite
+
+	# Flip
 	if direction > 0:
 		animated_sprite.flip_h = false
 	elif direction < 0:
 		animated_sprite.flip_h = true
-	
-	# Play animations
+
+	# Movement animations
 	if is_on_floor():
 		if direction == 0:
 			animated_sprite.play("idle")
@@ -35,7 +44,7 @@ func _physics_process(delta):
 			animated_sprite.play("run")
 	else:
 		animated_sprite.play("jump")
-	
+
 	# Apply movement
 	if direction:
 		velocity.x = direction * SPEED
@@ -43,3 +52,12 @@ func _physics_process(delta):
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
+	
+# Hit sound and animation 
+func die():
+	if is_dead:
+		return
+	is_dead = true
+	Engine.time_scale = 0.5 
+	hit_sound.play() 
+	animated_sprite.play("death_hit")
